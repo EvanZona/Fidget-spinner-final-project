e66c4a1f4bc89907932643c097d9da82fcfc69c9
Evan Zona <evanbzona@gmail.com>
2022-05-18 10:31:27 -0500

    Initial Commit

---
+++ Fidget spinner final project/Fidget_spinner_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fidget_spinner_final_projectApp.swift
+//  Fidget spinner final project
+//
+//  Created by Student on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Fidget_spinner_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
