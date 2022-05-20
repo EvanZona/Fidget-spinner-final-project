f1fbd56e381a3c019e6e5a6d1d5b131958cf0897
Evan Zona <evanbzona@gmail.com>
2022-05-20 10:21:06 -0500

    coded app

--- Fidget spinner final project/ContentView.swift
+++ Fidget spinner final project/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rotation = 0.0
+    @State private var dragVelocity = 0.0
+    @State private var dragDuration = 0.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Virtual Fidget Spinner")
+            .font(.title)
+        Spacer()
+        Image("Red Spinner")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300)
+            .rotationEffect(Angle(degrees: rotation))
+            .gesture(DragGesture(minimumDistance: 1, coordinateSpace: .local).onChanged({
+                value in
+                let valueDescription = "\(value)"
+                //intercept string
+                let start = valueDescription.range(of: "valuePerSecond: (")
+                let end = valueDescription.range(of: ")))")
+                let arr = String(valueDescription[(start!.upperBound)..<(end!.lowerBound)]).components(separatedBy: ",")
+                dragVelocity += Double(arr.first!)!
+                dragDuration += 1.0
+            }).onEnded({ value in
+                withAnimation(.easeOut(duration: 5)) {
+                    rotation -= dragVelocity / dragDuration
+                }
+                dragVelocity = 0.0
+                dragDuration = 0.0
+            }))
+        Spacer()
     }
 }
 
